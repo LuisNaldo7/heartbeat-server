1deb170d3d819172a76410271eb077645268093b
Nils Effinghausen <nils.effinghausen@gmail.com>
2021-04-03 15:46:47 +0200

    format

--- src/pulse/pulse.controller.ts
+++ src/pulse/pulse.controller.ts
@@ -8,45 +8,48 @@ const dbCon = mysql.createConnection({
   ssl: config.db.ssl,
   user: config.db.user,
   password: config.db.pw,
-  database: config.db.database
+  database: config.db.database,
 });
 
 function updateDevice(id: string, type: string) {
-  
-  var date = new Date().toISOString();
-  date = date.replace("Z", "");
+  let date = new Date().toISOString();
+  date = date.replace('Z', '');
 
-  var sql = "UPDATE devices " + 
-            "SET " + 
-              "last_seen = '" + date + "', " + 
-              "type = '" + type + "' " +
-            "WHERE " +
-            "guid = '" + id + "';";
+  const sql =
+    'UPDATE devices ' +
+    'SET ' +
+    "last_seen = '" +
+    date +
+    "', " +
+    "type = '" +
+    type +
+    "' " +
+    'WHERE ' +
+    "guid = '" +
+    id +
+    "';";
 
   dbCon.query(sql, (err, result) => {
     if (err) {
       console.log(err);
     }
   });
-
 }
 
 @Controller('pulse')
 export class PulseController {
-
   @Get('beat/:id')
-  async beat(@Param('id') id) {  
-    updateDevice(id, "BEAT");
+  async beat(@Param('id') id) {
+    updateDevice(id, 'BEAT');
   }
 
   @Get('boot/:id')
   async rise(@Param('id') id) {
-    updateDevice(id, "BOOT");
+    updateDevice(id, 'BOOT');
   }
 
   @Get('shutdown/:id')
   async die(@Param('id') id) {
-    updateDevice(id, "SHUTDOWN");
+    updateDevice(id, 'SHUTDOWN');
   }
-
 }
--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -10,52 +10,49 @@ const dbCon = mysql.createConnection({
   ssl: config.db.ssl,
   user: config.db.user,
   password: config.db.pw,
-  database: config.db.database
+  database: config.db.database,
 });
-
 
 @Controller('stats')
 export class StatsController {
-
   @Get('mail')
   async mail(): Promise<boolean> {
-    
-    var transporter = nodemailer.createTransport({
-        service: config.mail.host,
-        requireTLS: config.mail.require_tls,
-        auth: {
-          user: config.mail.from,
-          pass: config.mail.pw
-        }
-      });
-      
-    var mailOptions = {
+    const transporter = nodemailer.createTransport({
+      service: config.mail.host,
+      requireTLS: config.mail.require_tls,
+      auth: {
+        user: config.mail.from,
+        pass: config.mail.pw,
+      },
+    });
+
+    const mailOptions = {
       from: config.mail.from,
       to: config.mail.to,
       subject: 'Sending Email using Node.js',
-      text: 'That was easy!'
+      text: 'That was easy!',
     };
-    
+
     return new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, function(err, result) {
-        if (err) { reject(err); }
+      transporter.sendMail(mailOptions, function (err, result) {
+        if (err) {
+          reject(err);
+        }
         console.log('Email sent: ' + result.response);
         resolve(result);
-      })
+      });
     });
-
   }
 
   @Get('devices')
   async getDevicesx(): Promise<string> {
-
     return new Promise((resolve, reject) => {
-      dbCon.query("SELECT * FROM devices", function (err, result) {
-        if (err) { reject(err); }
+      dbCon.query('SELECT * FROM devices', function (err, result) {
+        if (err) {
+          reject(err);
+        }
         resolve(result);
-      })
+      });
     });
-
   }
-
 }

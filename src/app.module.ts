190ec5d87c8222c1bb5e277edb93c521fded9c5d
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-06-11 22:50:36 +0200

    remove template endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './application/app.service';
 import { PulseService } from './application/pulse.service';
 import { StatsService } from './application/stats.service';
 
-import { AppController } from './controller/app.controller';
 import { PulseController } from './controller/pulse.controller';
 import { StatsController } from './controller/stats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PulseController, StatsController],
-  providers: [AppService, PulseService, StatsService],
+  controllers: [PulseController, StatsController],
+  providers: [PulseService, StatsService],
 })
 export class AppModule {}
--- src/application/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AppServiceInterface } from './app.service.interface';
-
-@Injectable()
-export class AppService implements AppServiceInterface {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}

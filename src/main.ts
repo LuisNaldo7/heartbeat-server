3793de90ce9d9384b7adb490dc2382efaf457f5b
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-06-12 16:13:23 +0200

    add fallback values

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dotenv = require('dotenv');
+import { fallback } from './fallback-values';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.HEARTBEAT_SERVER_PORT || 3000);
+  await app.listen(
+    process.env.HEARTBEAT_SERVER_PORT || fallback.HEARTBEAT_SERVER_PORT,
+  );
 }
 
 dotenv.config();
--- src/sql.ts
+++ src/sql.ts
@@ -1,13 +1,16 @@
 import * as mysql from 'mysql';
+import { fallback } from './fallback-values';
 
 export function getConnection(): mysql.Connection {
   const con = mysql.createConnection({
-    host: process.env.HEARTBEAT_DB_HOST,
-    port: parseInt(process.env.HEARTBEAT_DB_PORT || '3306'),
-    ssl: JSON.parse(process.env.HEARTBEAT_DB_SSL || 'true'),
-    user: process.env.HEARTBEAT_DB_USER,
-    password: process.env.HEARTBEAT_DB_PASSWORD,
-    database: process.env.HEARTBEAT_DB_DATABASE,
+    host: process.env.HEARTBEAT_DB_HOST || fallback.HEARTBEAT_DB_HOST,
+    port: process.env.HEARTBEAT_DB_PORT || fallback.HEARTBEAT_DB_PORT,
+    ssl: JSON.parse(process.env.HEARTBEAT_DB_SSL || fallback.HEARTBEAT_DB_SSL),
+    user: process.env.HEARTBEAT_DB_USER || fallback.HEARTBEAT_DB_USER,
+    password:
+      process.env.HEARTBEAT_DB_PASSWORD || fallback.HEARTBEAT_DB_PASSWORD,
+    database:
+      process.env.HEARTBEAT_DB_DATABASE || fallback.HEARTBEAT_DB_DATABASE,
   });
 
   con.on('error', (err) => {

f689ee6d7aaa92c1bcca16948dc9065c9c541eef
Nils Effinghausen <nils.effinghausen@maibornwolff.de>
2021-06-12 12:10:45 +0200

    explicitly set bracket spacing for prettier

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-require('dotenv').config();
+import dotenv = require('dotenv');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.HEARTBEAT_SERVER_PORT || 3000);
 }
+
+dotenv.config();
 bootstrap();
